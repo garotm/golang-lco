0778d0e7a1f20cea4d2043771eedaee75e6e7754
garotm <gconklin@acp.us.com>
2021-12-17 14:02:49 -0500

    more examples

--- app1/main.go
+++ app1/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	const f_name, l_name = "Garot", "Conklin"
 	fmt.Println("Hello", f_name, l_name, "... Welcome to GO!")
+
+	str := "Garot Michael Conklin"
+	substr := "a"
+	occurance := strings.Count(str, substr)
+	length := len(str)
+	fmt.Println("The number of occurrences of the letter:", substr, ", in the string", str, "is: ", occurance)
+	fmt.Println("The length of the string is", length, "characters long.")
 
 	count := 100
 	b := "buzz"
