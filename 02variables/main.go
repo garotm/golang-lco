dc0613339b8140c4caa42825bf8b7ec7511fafd0
garotm <gconklin@acp.us.com>
2021-12-21 12:13:32 -0500

    additional 'stuff'

--- 02variables/main.go
+++ 02variables/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+const LoginToken string = "askdjfhkuyer/fhaerij" // Public/Global variable
+
 func main() {
 	var username string = "Garot"
 	fmt.Println(username)
@@ -14,4 +16,20 @@ func main() {
 	var isLoggedIn bool
 	fmt.Println(isLoggedIn)
 	fmt.Printf("Variable %v is of type: %T \n", isLoggedIn, isLoggedIn)
+
+	// var smallVal uint8 = 255
+	var smallVal byte = 254
+	fmt.Println(smallVal)
+	fmt.Printf("Variable %v is of type: %T \n", smallVal, smallVal)
+
+	var smallFloat float32 = 254.567238389
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable %v is of type: %T \n", smallFloat, smallFloat)
+
+	// default value is always 0
+	fmt.Printf("%v, %T\n", LoginToken, LoginToken)
+	// print out each letter in LoginToken
+	for i, letter := range LoginToken {
+		fmt.Println(i, string(letter))
+	}
 }
