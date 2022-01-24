e3d2b4b4aa161f2c9eab0db65a95ef1dba25e0ae
garotm <gconklin@acp.us.com>
2022-01-24 15:34:54 -0500

    chore: add another example of working with a file

--- everything/files/files.go
+++ everything/files/files.go
@@ -15,12 +15,18 @@ func main() {
 	file, err := os.Create("./my-lco-file.txt")
 	checkNilErr(err)
 	length, err := io.WriteString(file, content)
-
 	checkNilErr(err)
 	fmt.Println("length is: ", length)
 	defer file.Close()
-
 	readFile("./my-lco-file.txt")
+
+	anotherFile, err := os.Create("./another-lco-file.txt")
+	checkNilErr(err)
+	size, err := io.WriteString(anotherFile, welcome)
+	checkNilErr(err)
+	fmt.Println("size is: ", size)
+	defer anotherFile.Close()
+	readFile("./another-lco-file.txt")
 }
 
 func readFile(fileName string) {
