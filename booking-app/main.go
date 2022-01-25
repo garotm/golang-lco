c49e47473eb146826ae3dd0ce7e261d049218d45
garotm <gconklin@acp.us.com>
2022-01-25 16:46:19 -0500

    chore: move more things around and add inputs to function

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,15 +2,14 @@ package main
 
 import "fmt"
 
-var numberOfTickets uint
-
 func main() {
 
 	var conferenceName string = "Go Conference"
 	welcome := fmt.Sprintf("Welcome to the %v booking app!", conferenceName)
-	const conferenceTickets uint = 50 // we don't want this to change or be negative
+	const conferenceTickets = 50 // we don't want this to change or be negative
 	var remainingTickets uint = 50
 	var merchCount uint = 10
+	var numberOfTickets uint
 
 	fmt.Printf("conferenceTickets is of type %T\nremainingTickets is of type %T\nconferenceName is of type %T\n\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -18,23 +17,17 @@ func main() {
 	fmt.Printf("\nWe have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("We also have %v merch items left\n\n", merchCount)
 
-	getInput()
-
-	// update the number of tickets remaining
-	remainingTickets = conferenceTickets - numberOfTickets
-
-	fmt.Printf("There are now %v tickets remaining from the original %v we started with", remainingTickets, conferenceTickets)
+	getInput(numberOfTickets, conferenceTickets, remainingTickets)
 
 }
 
-func getInput() {
+func getInput(numberOfTickets, conferenceTickets, remainingTickets uint) {
 
 	var firstName string
 	var lastName string
 	var email string
 	prefix := "Please enter"
 
-	// ask user for their first/last name, email addr and number of tickets they want
 	fmt.Println(prefix, "your first name: ")
 	fmt.Scan(&firstName) // the '&' is a pointer
 
@@ -48,4 +41,8 @@ func getInput() {
 	fmt.Scan(&numberOfTickets)
 
 	fmt.Printf("Thank you %v %v for purchasing %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, numberOfTickets, email)
+
+	// update the number of tickets remaining
+	remainingTickets = conferenceTickets - numberOfTickets
+	fmt.Printf("There are now %v tickets remaining from the original %v we started with\n\n", remainingTickets, conferenceTickets)
 }
