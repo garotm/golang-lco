0d44f917c8be0707466a6540bc4997cd5f46e331
garotm <gconklin@acp.us.com>
2022-01-25 16:17:56 -0500

    chore: add a function and move some things around like global scoped vars

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,25 +2,36 @@ package main
 
 import "fmt"
 
+var numberOfTickets uint
+
 func main() {
 
 	var conferenceName string = "Go Conference"
 	welcome := fmt.Sprintf("Welcome to the %v booking app!", conferenceName)
-	const conferenceTickets = 50
-	var remainingTickets uint
+	const conferenceTickets uint = 50 // we don't want this to change or be negative
+	var remainingTickets uint = 50
 	var merchCount uint = 10
 
-	fmt.Printf("conferenceTickets is of type %T\nremainingTickets is of type %T\nconferenceName is of type %T\n", conferenceTickets, remainingTickets, conferenceName)
+	fmt.Printf("conferenceTickets is of type %T\nremainingTickets is of type %T\nconferenceName is of type %T\n\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Println(welcome)
-	fmt.Printf("\nWelcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("\nWe have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("We also have %v merch items left\n\n", merchCount)
+
+	getInput()
+
+	// update the number of tickets remaining
+	remainingTickets = conferenceTickets - numberOfTickets
+
+	fmt.Printf("There are now %v tickets remaining from the original %v we started with", remainingTickets, conferenceTickets)
+
+}
+
+func getInput() {
 
 	var firstName string
 	var lastName string
 	var email string
-	var numberOfTickets uint
 	prefix := "Please enter"
 
 	// ask user for their first/last name, email addr and number of tickets they want
@@ -36,10 +47,5 @@ func main() {
 	fmt.Println(prefix, "the number of tickets you'd like to purchase today: ")
 	fmt.Scan(&numberOfTickets)
 
-	// update the number of tickets remaining
-	remainingTickets = conferenceTickets - numberOfTickets
-
 	fmt.Printf("Thank you %v %v for purchasing %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, numberOfTickets, email)
-	fmt.Printf("There are now %v tickets remaining from the original %v we started with", remainingTickets, conferenceTickets)
-
 }
