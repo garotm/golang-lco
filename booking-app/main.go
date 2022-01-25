1ec7d2a4de5020ce45898ba1bc88264d9cc6ddde
garotm <gconklin@acp.us.com>
2022-01-25 15:42:15 -0500

    chore: minor updates in porgress to the booking-app

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,23 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var welcome string = "Welcome to the Go Conference booking app!"
-	conferenceName := "Go Conference"
+
+	var conferenceName string = "Go Conference"
+	welcome := fmt.Sprintf("Welcome to the %v booking app!", conferenceName)
 	const conferenceTickets = 50
-	var remainingTickets int
+	var remainingTickets uint
 	var merchCount uint = 10
 
 	fmt.Printf("conferenceTickets is of type %T\nremainingTickets is of type %T\nconferenceName is of type %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Println(welcome)
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("\nWelcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Printf("We also have %v merch items left\n", merchCount)
+	fmt.Printf("We also have %v merch items left\n\n", merchCount)
 
 	var firstName string
 	var lastName string
-	var emailAddr string
-	var numOfTickets int
+	var email string
+	var numberOfTickets uint
 	prefix := "Please enter"
 
 	// ask user for their first/last name, email addr and number of tickets they want
@@ -30,16 +31,15 @@ func main() {
 	fmt.Scan(&lastName)
 
 	fmt.Println(prefix, "your email address: ")
-	fmt.Scan(&emailAddr)
+	fmt.Scan(&email)
 
 	fmt.Println(prefix, "the number of tickets you'd like to purchase today: ")
-	fmt.Scan(&numOfTickets)
-
-	fmt.Printf("User %v %v with email address %v booked %v tickets.\n", firstName, lastName, emailAddr, numOfTickets)
+	fmt.Scan(&numberOfTickets)
 
 	// update the number of tickets remaining
-	remainingTickets = conferenceTickets - numOfTickets
+	remainingTickets = conferenceTickets - numberOfTickets
 
+	fmt.Printf("Thank you %v %v for purchasing %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, numberOfTickets, email)
 	fmt.Printf("There are now %v tickets remaining from the original %v we started with", remainingTickets, conferenceTickets)
 
 }
